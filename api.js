be037cc73074f9b94fe22cbac72151895506681e
didier_dev_raja <didiertuyizere326@gmail.com>
2026-09-24 19:57:40 +0200

    Add files via upload

---
+++ api.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MySQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "api"
+});
+//connection checking
+connection.connect((err) => {
+    if (err) {
+        console.error("Error connecting to MySQL:", err);
+        return;
+    }
+    console.log("Connected to MySQL database");
+});
+
+app.post("/insert", async (req, res) => {
+
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+    connection.query(sql, [name, email, hashedPassword], (err, result) => {
+        if (err) {
+            console.error("Error inserting data:", err);
+            res.status(500).json({ error: "Error inserting data" });
+        } else {
+            res.status(200).json({ message: "Data inserted successfully" });
+        }
+    });
+
+});
+
+app.get("/select", (req, res) => {
+    const sql = "SELECT * FROM users";
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error("Error fetching users:", err);
+            res.status(500).json({ error: "Error fetching users" });
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+app.put("/update/:id", async (req, res) =>{
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const sql = "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?";
+
+    connection.query(sql, [name, email, hashedPassword, id], (err, result) => {
+        if (err) {
+            console.error("Error updating user:", err);
+            res.status(500).json({ error: "Error updating user" });
+        } else {
+            res.status(200).json({ message: "User updated successfully" });
+        }
+    });
+});
+
+
+app.delete("/delete/:id", (req, res) => {
+    const { id } = req.params;
+    const sql = "DELETE FROM users WHERE id = ?";   
+     connection.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error("Error deleting user:", err); 
+            res.status(500).json({ error: "Error deleting user" });
+        } else {
+            res.status(200).json({ message: "User deleted successfully" });
+        }
+    });
+});
+// login 
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const sql = "SELECT * FROM users WHERE email = ?";  
+    connection.query(sql, [email], async (err, results) => {
+        if (err) {
+            console.error("Error fetching user:", err); 
+            res.status(500).json({ error: "Error fetching user" });
+        }   else {
+            req.session.user = results[0];
+            if (results.length > 0) {
+                const user = results[0];
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+                if (isPasswordValid) {
+                    res.status(200).json({ message: "Login successful" });
+                } else {
+                    res.status(401).json({ error: "Invalid password" });
+                }
+            } else {
+                res.status(404).json({ error: "User not found" });
+            }
+        }
+    });
+});
+//log out
+app.post("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Error logging out:", err);
+            res.status(500).json({ error: "Error logging out" });
+        } else {
+            res.status(200).json({ message: "Logout successful" });
+        }
+    });
+}
+)
+const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}
+)
